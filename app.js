3dd09017d36ad6279b78cef6b9b753012f54146d
Karan Rawat <karanrawat031@gmail.com>
2016-08-31 02:18:37 +0530

    Added choice feature and db integration

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var port    = process.env.PORT || 8080;
 
 mongoose.connect('mongodb://localhost/votingapp');
 
+var votingSchema = new mongoose.Schema({
+    question:String,
+    choice:[String]
+});
+
+var  Voting= mongoose.model('Voting',votingSchema);
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,7 +25,33 @@ app.get('/',function(req,res){
 
 //index route
 app.get('/home',function(req,res){
-   res.send('Index Page');
+   res.render('index');
+});
+
+//all polls get route
+app.get('/polls',function(req,res){
+   Voting.find({},function(err,found){
+     if(err){
+       console.log(err);
+       res.redirect('/polls');
+     }else{
+       res.render('polls',{found:found});
+     }
+   });
+});
+
+//adding post to db
+app.post('/polls',function(req,res){
+  var question = req.body.question;
+  var choice = req.body.choice;
+  Voting.create({question:question,choice:choice},function(err,created){
+    if(err){
+      console.log(err);
+      res.redirect('/polls');
+    }else{
+      res.redirect('/polls');
+    }
+  });
 });
 
 app.listen(port);
