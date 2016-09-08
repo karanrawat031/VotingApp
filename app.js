68d0dbb00a37dc0c0f3e7a96cf6d58c6546c5dc7
Karan Rawat <karanrawat031@gmail.com>
2016-09-09 02:56:06 +0530

    Added Authentication using passportjs

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport    = require('passport');
 var port    = process.env.PORT || 8080;
 var methodOverride = require('method-override');
+var LocalStrategy = require('passport-local');
+var router = express.Router();
+var passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect('mongodb://localhost/votingapp');
 
@@ -25,13 +29,40 @@ const PollSchema = new Schema({
   options : [OptionSchema]
 });
 
+//user schema
+var UserSchema = new Schema({
+    username: String,
+    email:String,
+    password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
 const Poll = mongoose.model('Poll', PollSchema);
 const Option = mongoose.model('Option', OptionSchema);
+const User = mongoose.model('User', UserSchema);
+
+
+app.use(require("express-session")({
+    secret: "Hello There",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 //landing route
 app.get('/',function(req,res){
@@ -91,7 +122,7 @@ app.get('/polls/show/:id',function(req,res){
 });
 
 //adding vote to particular choice of a particular post
-app.put('/polls/show/:id',function(req,res){
+app.put('/polls/show/:id',isLoggedIn,function(req,res){
 
       Poll.findById(req.params.id,function(err,found){
         if(err){
@@ -116,48 +147,49 @@ app.put('/polls/show/:id',function(req,res){
 
 });
 
-// app.put('/polls/show/:id',function(req,res){
-//   var foundC = req.body.choices;
-//   console.log(foundC);
-//     var updatedCamp = {};
-//     Campground.findByIdAndUpdate(req.params.id,updatedCamp,function(err,updatedCampground){
-//         if(err){
-//             console.log('Error found');
-//             console.log(err);
-//             res.redirect('/campgrounds');
-//         }else{
-//             function findOp(ch) { 
-//                   return ch.text == foundC;
-//               }
-//             var selected = option.options.find(findOp);
-//             res.redirect('/campgrounds/'+req.params.id);    
-//         }
-//     });
-// });
-      // var foundC = req.body.choices;
-      // console.log(foundC);
-      //     Poll.findById(req.params.id,function(err,option){
-      //       if(err){
-      //         console.log(err);
-      //         res.redirect('back');
-      //       }else{
-      //         function findOp(ch) { 
-      //             return ch.text == foundC;
-      //         }
-      //         var selected = option.options.find(findOp);
-      //         selected.votes +=1;
-      //         console.log(selected.id); 
-      //         Option.findByIdAndUpdate(selected.id,selected.votes,function(err,updated){
-      //           if(err){
-      //             console.log(err);
-      //             res.redirect('back');
-      //           }else{
-      //             console.log(updated);
-      //             res.redirect('back');
-      //           }
-      //         });
-      //       }
-      //       });
+//showing the register
+app.get('/register',function(req,res){
+  res.render('register');
+});
+
+//Into the database
+app.post('/register',function(req,res){
+   var newUser = new User({username: req.body.username,email:req.body.email});
+   User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/"); 
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+// handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/polls",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/polls");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port);
 console.log('The magic happens on port ' + port);
